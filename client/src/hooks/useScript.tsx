93c4ea81c7fb98fad0aff46004e742787752933b
LRunLin <liurl0621@gmail.com>
2022-02-01 22:30:29 +0800

    修复
    修复切换页面或者视图更新会重复创建统计访问量的script的BUG（加了个memo）

--- client/src/hooks/useScript.tsx
+++ client/src/hooks/useScript.tsx
@@ -1,5 +1,4 @@
 import { useEffect } from "react";
-import type { FunctionComponent } from "react";
 
 /**
  * todo 创建一个script标签引入外部JS文件
@@ -7,16 +6,13 @@ import type { FunctionComponent } from "react";
  * @params watch {any} 依赖项-根据某个依赖创建
  * @return id {string} ID-script的ID属性
  */
-const useScript = (src: string, watch?: any) => {
+const useScript = (src: string, watch?: any[]) => {
   let id = "use" + Math.random().toString(32).substring(3, 7) + "script";
-  useEffect(
-    () => {
-      let script = document.createElement("script");
-      script.id = id;
-      script.src = src;
-      document.head.append(script);
-    },
-    watch ? [watch] : []
-  );
+  useEffect(() => {
+    let script = document.createElement("script");
+    script.id = id;
+    script.src = src;
+    document.head.append(script);
+  }, watch || []);
 };
 export default useScript;
--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,27 +1,37 @@
 import type { AppProps } from "next/app";
-
+import Script from "next/script";
 // import "antd/dist/antd.css";
+//部分动画样式在这个css里面
 import "antd/lib/style/index.css";
 
 import moment from "moment";
 import "moment/locale/zh-cn";
 moment.locale("zh-cn");
-import Script from "next/script";
+import { memo } from "react";
 
 import Store from "@/store";
 import Style from "@/style/global";
 import "@/utils/axios";
 
-let scriptSrc =
-  process.env.NODE_ENV == "development" ? "http://localhost:3000/" : "https://blog-api.blogweb.cn/";
+/**
+ * 在页面加载完毕时创建一个js统计访问量，打印log
+ */
+let MemoScript = memo(() => {
+  let scriptSrc =
+    process.env.NODE_ENV == "development"
+      ? "http://localhost:3000/"
+      : "https://blog-api.blogweb.cn/";
+  return <Script src={`${scriptSrc}statistics?v${+new Date()}`} strategy="lazyOnload" />;
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Style />
-        <Store>
-          <Component {...pageProps} />
-        </Store>
-      <Script src={`${scriptSrc}statistics?v${+new Date()}`} strategy="lazyOnload" />
+      <Store>
+        <Component {...pageProps} />
+      </Store>
+      <MemoScript />
     </>
   );
 }
