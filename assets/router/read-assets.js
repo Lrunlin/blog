2cee785b131abbe965845ab444ed742a74e864be
LRunLin <liurl0621@gmail.com>
2022-04-02 16:29:44 +0800

    Revert "新增+修复资源管理"
    This reverts commit 72106e32e1ed340d0b8656b282e19ae12f6b43da.

--- assets/router/read-assets.js
+++ assets/router/read-assets.js
@@ -5,8 +5,6 @@ const fs = require('fs');
 const moment = require('moment');
 const path = require('path');
 let dir = path.join(__dirname, '../public/image');
-let face = path.join(__dirname, '../public/face');
-let github = path.join(__dirname, '../public/github');
 
 router.get('/assets', async (req, res) => {
 
@@ -23,44 +21,23 @@ router.get('/assets', async (req, res) => {
         return result;
     }
     //获取正式文件夹内所有图片
-    let dirData = fs.readdirSync(dir).filter(item => item != '.gitkeep').map(item => {
-        return {
-            name: item,
-            dir: dir,
-            type: 'image'
-        }
-    });
-    let faceData = fs.readdirSync(face).filter(item => item != '.gitkeep').map(item => {
-        return {
-            name: item,
-            dir: face,
-            type: 'face'
-        }
-    });
-    let githubData = fs.readdirSync(github).filter(item => item != '.gitkeep').map(item => {
-        return {
-            name: item,
-            dir: github,
-            type: 'github'
-        }
-    });
-    let data = [...dirData, ...faceData, ...githubData].map(item => {
+    let dirData = fs.readdirSync(dir).filter(item => item != '.gitkeep');
+    let data = dirData.map(item => {
         //获取图片信息
-        let fileData = fs.statSync(`${item.dir}/${item.name}`);
+        let fileData = fs.statSync(`${dir}/${item}`);
         // 返回格式
         return {
-            name: item.name,
+            name: item,
             size: formatSize(fileData.size),
-            time: moment(fileData.birthtime).format('yyyy-MM-DD hh:mm'),
-            type: item.type
+            time: moment(fileData.birthtime).format('yyyy-MM-DD hh:mm')
         }
-    }).sort((a, b) => {
-        return +new Date(b.time) - (+new Date(a.time))
     });
+
     res.json({
         success: true,
         message: '查询静态文件',
         data: data
     });
+
 })
 module.exports = router;
