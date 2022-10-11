33edfdddc28118467c280abddde2dbdc8177f233
Liu Run Lin <liurl0621@gmail.com>
2022-10-11 22:31:30 +0800

    🐞 fix(用户排行中的联表查询):

--- server/src/routes/ranking/author.ts
+++ server/src/routes/ranking/author.ts
@@ -5,8 +5,6 @@ import Models from "@/db";
 import moment from "moment";
 let router = new Router();
 
-Models.User.belongsTo(Models.Article, { as: "author_data", foreignKey: "author", targetKey: "id" });
-
 router.get("/author-ranking", async ctx => {
   let time = moment(+new Date() - 604_800_000).format("YYYY-MM-DD hh:mm:ss");
   let data = await DB.User.findAll({
@@ -40,3 +38,4 @@ router.get("/author-ranking", async ctx => {
   };
 });
 export default router;
+// SELECT `id`, `name`, `auth`, `avatar_file_name`, `description`, `unit`, `location`, (select count(id) from article where article.author=user.id and article.state=1 and article.create_time>"2022-01-04") AS `article_count` FROM `user` AS `User` ORDER BY `article_count` DESC LIMIT 0, 5;
