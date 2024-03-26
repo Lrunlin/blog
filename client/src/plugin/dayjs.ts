b9a555793307bdc88c53c559f96fdf7dec67c21c
Lrunlin <liurl0621@gmail.com>
2024-03-26 21:11:46 +0800

    🐞 fix(修复日期选择器弹出错误):

--- client/src/plugin/dayjs.ts
+++ client/src/plugin/dayjs.ts
@@ -1,7 +1,10 @@
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
-import "dayjs/locale/zh-cn"; 
+import "dayjs/locale/zh-cn";
+import weekday from "dayjs/plugin/weekday";
+import localeData from "dayjs/plugin/localeData";
 
 dayjs.extend(relativeTime);
-dayjs.locale("zh-cn"); 
-
+dayjs.locale("zh-cn");
+dayjs.extend(weekday);
+dayjs.extend(localeData);
