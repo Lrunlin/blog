c94104414e50e1791006b8d1f6798b05f8adc1c5
Lrunlin <liurl0621@gmail.com>
2025-01-29 10:20:02 +0800

    🐞 fix(推荐缓存bug):

--- server/src/common/tasks/recommend/cache-recommend-data.ts
+++ server/src/common/tasks/recommend/cache-recommend-data.ts
@@ -14,29 +14,27 @@ async function setCacheRecommendData() {
       await redis.del(item);
     }
   }
-
-  await sleep(200);
-
+  const pipeline = await redis.pipeline();
   let tags = await DB.Tag.findAll({
     attributes: ["id", "belong_id"],
     raw: true,
   });
 
   let type = Object.keys(sort);
-  console.time("a");
-
   for (let index = 0; index < type.length; index++) {
     const t = type[index] as unknown as keyof typeof sort;
-    let { total, list } = await getArticleListData(1, t, {}, 1000);
+    let { total, list } = await getArticleListData(1, t, {}, 200);
+    await sleep(1000);
 
     for (let index = 0; index < list.length; index += 10) {
-      await redis.set(
+      await pipeline.set(
         `recommend-cache-${t}-${index / 10 + 1}`,
         JSON.stringify({ total, list: list.slice(index, index + 10) }),
       );
     }
     for (let index = 0; index < tags.length; index += 10) {
       const tag = tags[index];
+
       let { total, list } = await getArticleListData(
         1,
         t,
@@ -44,15 +42,17 @@ async function setCacheRecommendData() {
           tag: tag.belong_id ? tag.id : undefined,
           type: tag ? tag.id : undefined,
         }),
-        1000,
+        200,
       );
-      await redis.set(
-        `recommend-cache-${t}-${tag.id}-${index / 10 + 1}`,
-        JSON.stringify({ total, list: list.slice(index, index + 10) }),
-      );
+      for (let index = 0; index < list.length; index += 10) {
+        await pipeline.set(
+          `recommend-cache-${t}-${tag.id}-${index / 10 + 1}`,
+          JSON.stringify({ total, list: list.slice(index, index + 10) }),
+        );
+      }
     }
   }
-  console.timeEnd("a");
+  await pipeline.exec();
 }
 
 export default setCacheRecommendData;
